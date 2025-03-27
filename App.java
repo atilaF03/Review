3cf1ddaadea6316dbe7e6d9a429df2b5b6d10151
Átila <144128328+atilaF03@users.noreply.github.com>
2025-03-26 21:23:49 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,8 @@
+package Questao1;
+
+public class App {
+  public static void main(String[] args) {
+   Produtos produtos = new Produtos("camisetas", 200, 2);
+   produtos.informacoes();
+  }  
+}
---
+++ Produtos.java
@@ -0,0 +1,50 @@
+package Questao1;
+
+public class Produtos {
+    private String nome;
+    private double preco, quantidade;
+
+    public Produtos(String nome, double preco, double quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public double getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(double quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double calcularValorTotal() {
+        return getPreco() * getQuantidade();
+    }
+
+    public void informacoes() {
+    System.out.println("produtos");
+    System.out.println(" o nome"+ getNome());
+    System.out.println(" o nome"+ getPreco());
+    System.out.println(" o nome"+ getQuantidade());
+    double total = calcularValorTotal();
+    System.out.println("total:"+total);
+    }
+
+}
